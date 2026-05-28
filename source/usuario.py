3803bda77107b410537c9294c8965d0bf6cd2e90
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-28 19:32:02 -0300

    fix: place `Usuario` inside file `usuario.py`

---
+++ source/usuario.py
@@ -0,0 +1,8 @@
+class Usuario():
+    def __init__(self, id_usuario, nome, username, data_nascimento, contato, senha):
+        self.id = id_usuario
+        self.nome = nome
+        self.username = username
+        self.data_nasc = data_nascimento
+        self.contato = contato
+        self.senha = senha
