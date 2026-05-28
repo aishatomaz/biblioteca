ab888a3e8196ca13e48d547e067f8d09c2e1e0b1
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-28 19:16:09 -0300

    feat(livro): add class `Livro`

---
+++ source/livro.py
@@ -0,0 +1,8 @@
+class Livro:
+
+    def __init__(self, id_livro, paginas, categoria, autor, disponivel):
+        self.id_livro = id_livro
+        self.paginas = paginas
+        self.categoria = categoria
+        self.autor = autor
+        self.disponivel = disponivel
