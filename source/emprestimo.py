f33d9cf3f19bc4fb6de0140e4b60d467b340c499
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-29 14:34:48 -0300

    fix: align constructor attributes with class members and add missing attributes

--- source/emprestimo.py
+++ source/emprestimo.py
@@ -3,6 +3,6 @@ class Emprestimo:
         Classe base para emprestimos.
     """
     def __init__(self, devolucao, livro, usuario):
-        self.devolucao = id_devolucao
+        self.devolucao = devolucao
         self.livro = livro
         self.usuario = usuario
--- source/livro.py
+++ source/livro.py
@@ -1,7 +1,8 @@
 class Livro:
 
-    def __init__(self, id_livro, paginas, categoria, autor, disponivel):
+    def __init__(self, id_livro, titulo, paginas, categoria, autor, disponivel):
         self.id_livro = id_livro
+        self.titulo = titulo
         self.paginas = paginas
         self.categoria = categoria
         self.autor = autor
